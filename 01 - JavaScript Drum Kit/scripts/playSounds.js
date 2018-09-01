e508155cadebcec15df942e6825af369d16dd528
George Harris <citizenharris@gmail.com>
2018-09-01 11:56:01 +0100

    Separate script to different file

---
+++ 01 - JavaScript Drum Kit/scripts/playSounds.js
@@ -0,0 +1,19 @@
+const removeTransition = (e) => {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
+
+const playSound = (e) => {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"`);
+	if (!audio) return;
+
+	key.classList.add('playing');
+
+	audio.currentTime() = 0; // rewind audio
+	audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
