d80d86d8aea240f854740117737d216d4547d590
James Pickett <james.pickett@cambridgeshire.gov.uk>
2018-09-07 17:39:41 +0100

    Day 7 Bulk copied remaining starting files

---
+++ makecopies.bat
@@ -0,0 +1,66 @@
+cd "10 - Hold Shift and Check Checkboxes"
+cp index-START.html index-JDP.html
+cd ..
+
+
+
+cd "11 - Custom Video Player"
+cp index-START.html index-JDP.html
+cd ..
+cd "12 - Key Sequence Detection"
+cp index-START.html index-JDP.html
+cd ..
+cd "13 - Slide in on Scroll"
+cp index-START.html index-JDP.html
+cd ..
+cd "14 - JavaScript References VS Copying"
+cp index-START.html index-JDP.html
+cd ..
+cd "15 - LocalStorage"
+cp index-START.html index-JDP.html
+cd ..
+cd "16 - Mouse Move Shadow"
+cp index-START.html index-JDP.html
+cd ..
+cd "17 - Sort Without Articles"
+cp index-START.html index-JDP.html
+cd ..
+cd "18 - Adding Up Times with Reduce"
+cp index-START.html index-JDP.html
+cd ..
+cd "19 - Webcam Fun"
+cp index-START.html index-JDP.html
+cd ..
+cd "20 - Speech Detection"
+cp index-START.html index-JDP.html
+cd ..
+cd "21 - Geolocation"
+cp index-START.html index-JDP.html
+cd ..
+cd "22 - Follow Along Link Highlighter"
+cp index-START.html index-JDP.html
+cd ..
+cd "23 - Speech Synthesis"
+cp index-START.html index-JDP.html
+cd ..
+cd "24 - Sticky Nav"
+cp index-START.html index-JDP.html
+cd ..
+cd "25 - Event Capture, Propagation, Bubbling and Once"
+cp index-START.html index-JDP.html
+cd ..
+cd "26 - Stripe Follow Along Nav"
+cp index-START.html index-JDP.html
+cd ..
+cd "27 - Click and Drag"
+cp index-START.html index-JDP.html
+cd ..
+cd "28 - Video Speed Controller"
+cp index-START.html index-JDP.html
+cd ..
+cd "29 - Countdown Timer"
+cp index-START.html index-JDP.html
+cd ..
+cd "30 - Whack A Mole"
+cp index-START.html index-JDP.html
+cd ..
