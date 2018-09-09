d6f4d354553de7facd196cdc1caabbb09814fd39
George Harris <citizenharris@gmail.com>
2018-09-09 15:55:39 +0100

    Finish autocomplete

--- 06 - Type Ahead/autocomplete.js
+++ 06 - Type Ahead/autocomplete.js
@@ -1,2 +1,40 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const cities = [];
+
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+
+function findMatches(input, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(input, 'gi');
+    return place.city.match(regex) || place.state.match(regex)
+  })
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const result = findMatches(this.value, cities);
+  const html = result.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const city = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const state = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${city}, ${state}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('keyup', displayMatches);
