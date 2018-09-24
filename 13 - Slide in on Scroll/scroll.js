273f8716020d4027a051139522a8dbcecfeb9b0a
citizenharris <citizenharris@gmail.com>
2018-09-24 17:21:29 +0100

    Format comment

--- 13 - Slide in on Scroll/scroll.js
+++ 13 - Slide in on Scroll/scroll.js
@@ -7,7 +7,7 @@ const options = {
 function callback(images, observer) {
 	images.forEach(image => {
 		if (image.intersectionRatio > 0.5) {
-			image.target.classList.add('active') // `target` is needed because image is actually an IntersectionObserverEntry object, the target attribute accesses the img 
+			image.target.classList.add('active') // `target` is needed because `image` is actually an IntersectionObserverEntry object, the target attribute accesses the img 
 		} else {
 			image.target.classList.remove('active')
 		}
