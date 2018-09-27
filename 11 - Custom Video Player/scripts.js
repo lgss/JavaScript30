c3cdaa1ed87bdf606d0e74b1ee493653e785a8ce
George Harris <citizenharris@gmail.com>
2018-09-27 16:25:40 +0100

    Finish video styling

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -16,8 +16,40 @@ function togglePlayButton() {
   toggle.innerText = icon;
 }
 
+function skip() {
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', togglePlayButton);
 video.addEventListener('pause', togglePlayButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(skipButton => {
+  skipButton.addEventListener('click', skip);
+})
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
